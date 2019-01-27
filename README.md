66f3119556866d9fb8f2e17a7c2f56e8f515fc73
ax1ine <axline@mail.ru>
2019-01-27 01:29:26 +0000

    Add autogenerated files to build GSW_BinaryBuilder

---
+++ README.md
@@ -0,0 +1,9 @@
+# GSW
+
+[![Build Status](https://travis-ci.org/ax1ine/GSW.svg?branch=master)](https://travis-ci.org/ax1ine/GSW)
+
+This repository builds binary artifacts for the GSW_BinaryBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/ax1ine/GSW/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,53 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "GSW_BinaryBuilder"
+version = v"0.1.0"
+
+# Collection of sources required to build GSW_BinaryBuilder
+sources = [
+    "https://github.com/ax1ine/GSW_BinaryBuilder.git" =>
+    "07a2c32978fe20a3e8791038207d9a57b2c5d0ff",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd GSW_BinaryBuilder
+make all
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libgswteos", :libgswteos)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
